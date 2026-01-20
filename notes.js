d01789a66a313e2bb210eb671e2f7745f5548938
ccharli33 <charliert20x@gmail.com>
2026-01-20 18:29:59 +0000

    Added topics, groups to notes

--- notes.js
+++ notes.js
@@ -5,25 +5,36 @@ fetch('notes.json')
     .then(data => {
         const container = document.getElementById('notesContainer');
 
-        data.notes.forEach(note => {
-            const box = document.createElement('div');
-            box.className = 'note';
+        data.topics.forEach(topic => {
+            // Topic title
+            const topicDiv = document.createElement('div');
+            topicDiv.className = 'topic';
 
-            box.innerHTML = `
-                <div class="card">
-                    <div class="first-content">
-                        <p>${note.title}</p>
-                    </div>  
-                    <div class="second-content">
-                        <p>${note.content}</p>
+            topicDiv.innerHTML = `<h2>${topic.name}</h2>`;
+            container.appendChild(topicDiv);
+
+            // Notes inside topic
+            topic.notes.forEach(note => {
+                const box = document.createElement('div');
+                box.className = 'note';
+
+                box.innerHTML = `
+                    <div class="card">
+                        <div class="first-content">
+                            <p>${note.title}</p>
+                        </div>
+                        <div class="second-content">
+                            <p>${note.content}</p>
+                        </div>
+                        <div class="second-content">
+                            <p>${note.date}</p>
+                        </div>
                     </div>
-                    <div class="second-content">
-                        <p>${note.date}</p>
-                    </div>
-                </div>
-            `
-            
-            container.appendChild(box);
+                `;
+
+                topicDiv.appendChild(box);
+            });
         });
     })
     .catch(error => console.error('Error loading notes:', error));
+
